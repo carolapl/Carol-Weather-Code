bbd468b632658f99e598ce9fb973a1cbd30c974a
carolapl <104767876+carolapl@users.noreply.github.com>
2022-06-04 00:33:53 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-
+let apiKey = "dac639384c9295f10a0318ea3dfcd2a6";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let city = "Singapore";
+console.log(apiUrl);
