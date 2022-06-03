2917dcd800760c360caeb8f1d3b9ee13770ba998
carolapl <104767876+carolapl@users.noreply.github.com>
2022-06-04 01:08:04 +0800

    update JS interface

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement = response.data.weather[0].description;
+}
+
 let apiKey = "dac639384c9295f10a0318ea3dfcd2a6";
+let city = "singapore";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-let city = "Singapore";
-console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
